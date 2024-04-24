4a6f5fc343fe5d5fb585a795de7079a91a64ba7c
matteocaloia <164690390+matteocaloia@users.noreply.github.com>
2024-04-24 00:19:52 +0000

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
-  get("/directors", { :controller => "directors", :action => "index"})
+  get("/directors", {:controller => "directors", :action => "index"})
+
+  get("/directors/youngest", {:controller => "directors", :action => "youngest"})
+
+  get("/directors/eldest", {:controller => "directors", :action => "eldest"})
+
+  get("/directors/:director_id", {:controller => "directors", :action => "show"})
+
+  get("/movies", {:controller => "movies", :action => "index"})
+
+  get("/movies/:movie_id", {:controller => "movies", :action => "show"})
+
+  get("/actors", {:controller => "actors", :action => "index"})
+
+  get("/actors/:actor_id", {:controller => "actors", :action => "show"})
+
 end
+
